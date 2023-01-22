2fc8e08ff714d7d5e696a2af5701f5ead5ee4f03
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-01-21 21:01:26 -0600

    Updating the project

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,20 +5,13 @@ import Card from "./Card.js";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
-
-    const clickHendler = () => {
-        setTitle("Updated!");
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick={clickHendler}>Change Title</button>
         </Card>
     );
 }
