52cf7ade946c5ee29d66e868f913f7cb422e0989
Yevgen Sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-01-25 16:03:19 -0600

    feature of adding a single task

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -14,7 +14,7 @@ const NewExpense = (props) => {
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpanseData={saveExpenseDataHandler} />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
     );
 };
